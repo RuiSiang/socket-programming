4ad0241d9d5d0f01e9d2f427b6b1d4de1f2aa50d
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-12-11 19:32:24 +0800

    logger append

--- client/logger.cpp
+++ client/logger.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 void error(string message){
-  ofstream fout("error.log");
+  ofstream fout("error.log", ofstream::app);
   fout << message;
   fout.close();
 }
 
 void info(string message){
-  ofstream fout("info.log");
+  ofstream fout("info.log", ofstream::app);
   fout << message;
   fout.close();
 }
